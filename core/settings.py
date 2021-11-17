2ba0392aa503b318b9606853e42c777ba4995d28
SeemsChill <ilovesaigonbefore1975@gmail.com>
2021-11-17 14:45:59 +0700

    Replace all the source codes.

--- core/settings.py
+++ core/settings.py
@@ -170,12 +170,6 @@ SESSION_COOKIE_SECURE = True
 #    "https://saigon-by-night.vercel.app"
 #]
 
-CORS_ALLOWED_ORIGINS = [
-    "saigon-by-night.vercel.app"
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
